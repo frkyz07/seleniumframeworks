f5811e657be71b203932ef0d7ef478752213c2fd
faruk ayaz <46282512+frkyz07@users.noreply.github.com>
2022-06-04 12:07:57 +0300

    - extent reports

---
+++ mavenproject/src/test/java/rahulshattyacademy/mavenproject/ExtentReportDemo.java
@@ -0,0 +1,49 @@
+package rahulshattyacademy.mavenproject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+
+
+public class ExtentReportDemo {
+	
+	ExtentReports extent;
+	ExtentSparkReporter reporter;
+	
+	@Test
+	public void initialDemo()
+	{
+		ExtentTest test = extent.createTest("initialDemo");
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\frkyz\\OneDrive\\Belgeler\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com");
+		System.out.println(driver.getTitle());
+		driver.close();
+		extent.flush();
+	}
+	
+	@BeforeTest
+	public void config()
+	{
+		// ExtenReports, ExtentSparkReporter
+		String path = System.getProperty("user.dir")+"//reports//index.html";
+		reporter = new ExtentSparkReporter(path);
+		reporter.config().setReportName("Web Automation Results");
+		reporter.config().setDocumentTitle("Test Results");
+		
+		extent = new ExtentReports();
+		extent.attachReporter(reporter);
+		extent.setSystemInfo("Tester", "Faruk Ayaz");
+		
+		
+	}
+
+	
+
+}
