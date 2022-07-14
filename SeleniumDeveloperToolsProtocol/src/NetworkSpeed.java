70cea5e9b94717f78b3ab13775751b834bc2ebb0
faruk ayaz <46282512+frkyz07@users.noreply.github.com>
2022-07-14 15:35:47 +0300

    - managing network speed example

--- SeleniumDeveloperToolsProtocol/src/NetworkSpeed.java
+++ SeleniumDeveloperToolsProtocol/src/NetworkSpeed.java
@@ -18,7 +18,7 @@ public class NetworkSpeed {
 		devTools.createSession();
 		
 		devTools.send(Network.enable(Optional.empty(), Optional.empty(), Optional.empty()));
-		devTools.send(Network.emulateNetworkConditions(true, 3000, 20000, 100000, Optional.of(ConnectionType.ETHERNET)));
+		devTools.send(Network.emulateNetworkConditions(false, 3000, 20000, 100000, Optional.of(ConnectionType.ETHERNET)));
 		
 		devTools.addListener(Network.loadingFailed(), loadingFailed->{
 			
